9f283f03b752d96aaf2a52ba2d526a9c957b2dc0
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-10-14 13:45:35 +0300

    initial commit

---
+++ src/main/java/com/Database_layer/Route.java
@@ -0,0 +1,14 @@
+package com.Database_layer;
+
+public class Route {
+
+    private Integer routeId;
+
+    public Integer getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(Integer routeId) {
+        this.routeId = routeId;
+    }
+}
