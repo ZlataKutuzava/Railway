5985a52afc2e6ccaa841d89b07b88f7c5c814c93
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-10-14 15:21:30 +0300

    add hibernate and configure web.xml

--- src/main/java/com/Database_layer/Route.java
+++ src/main/java/com/Database_layer/Route.java
@@ -3,6 +3,42 @@ package com.Database_layer;
 public class Route {
 
     private Integer routeId;
+    private Integer trainId;
+    private Integer stationId;
+    private String timeArrival;
+    private String timeDeparture;
+
+    public String getTimeDeparture() {
+        return timeDeparture;
+    }
+
+    public void setTimeDeparture(String timeDeparture) {
+        this.timeDeparture = timeDeparture;
+    }
+
+    public String getTimeArrival() {
+        return timeArrival;
+    }
+
+    public void setTimeArrival(String timeArrival) {
+        this.timeArrival = timeArrival;
+    }
+
+    public Integer getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(Integer stationId) {
+        this.stationId = stationId;
+    }
+
+    public Integer getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(Integer trainId) {
+        this.trainId = trainId;
+    }
 
     public Integer getRouteId() {
         return routeId;
---
+++ src/main/java/com/Web_layer/IndexController.java
@@ -0,0 +1,17 @@
+package com.Web_layer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return new ModelAndView("index.jsp");
+    }
+}
